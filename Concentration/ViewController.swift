10f130eba6faf0e3cc892e895cf58d02ffc27e2e
Paulig <pavel.senchenko@gmail.com>
2018-09-01 21:28:51 +0300

    One Card is Flips Over

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -10,6 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
-
+    @IBAction func touchCard(_ sender: UIButton) {
+        
+        flipCard(withEmoji: "👻", on: sender)
+   
+    }
+    
+    func flipCard (withEmoji emoji: String, on button: UIButton) {
+        
+        if button.currentTitle == emoji {
+            button.setTitle("", for: UIControlState.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+        } else {
+            button.setTitle("👻", for: UIControlState.normal)
+            button.backgroundColor = #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 1)
+        }
+    }
+    
 }
 
