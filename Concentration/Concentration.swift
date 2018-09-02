94584c64ee0a2a89f81470fe13fb2a08af63a463
Paulig <pavel.senchenko@gmail.com>
2018-09-02 15:57:29 +0300

    Added check for matched cards. The game is works

--- Concentration/Concentration.swift
+++ Concentration/Concentration.swift
@@ -9,14 +9,28 @@
 import Foundation
 class Concentration
 {
-    
     var cards = [Card]()
     
+    var indexOfOneAndOnlyFaceUpCard: Int?
+    
     func choseCard (at index: Int) {
-        if cards[index].isFaceUp {
-            cards[index].isFaceUp = false
-        }else {
-            cards[index].isFaceUp = true
+        
+        if !cards[index].isMatched {
+            if let machIndex = indexOfOneAndOnlyFaceUpCard, machIndex != index {
+                if cards[machIndex].identifier == cards[index].identifier {
+                    cards[index].isMatched = true
+                    cards[machIndex].isMatched = true
+                }
+                cards[index].isFaceUp = true
+                indexOfOneAndOnlyFaceUpCard = nil
+            } else {
+                for flipdownIndex in cards.indices {
+                    cards[flipdownIndex].isFaceUp = false
+                }
+                cards[index].isFaceUp = true
+                indexOfOneAndOnlyFaceUpCard = index
+            }
+            
         }
         
     }
