f8a02a3164fc961c9541f2a38506ed588654f1ff
Paulig <pavel.senchenko@gmail.com>
2018-09-12 16:17:21 +0300

    Now Added color palette to Themes for game. Added theme title and when pressed new game, UI also changed Title, background color, primary color and emojies.

--- Concentration/Theme.swift
+++ Concentration/Theme.swift
@@ -8,44 +8,52 @@
 
 import UIKit
 
-private struct Theme {
-    var themePackName: String
+struct Theme {
+    var themeName: String
     var emojiPack: [String]
-    var cardBackColor: UIColor
-    var deskTopColor: UIColor
+    var primaryColor: UIColor
+    var backgroundColor: UIColor
     
 }
-private let    emojiPacks :[Theme] = [
-    Theme (themePackName: "Animal",
+ let    theme :[Theme] = [
+    
+    Theme (themeName: "Hellowen",
+           emojiPack: ["🧛‍♀️","🧟‍♀️","🎃","🧟‍♂️","🕷","🍭","💀","🤡","👁","🦇","😱","🙀"],
+           primaryColor: #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1),
+           backgroundColor: #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+    ),
+    
+    
+    Theme (themeName: "Animal",
            emojiPack: ["🦆","🐛","🦋","🐝","🐌","🐞","🐬","🐳","🐋","🦈","🐊","🐢"],
-           cardBackColor: #colorLiteral(red: 0.1960784346, green: 0.3411764801, blue: 0.1019607857, alpha: 1),
-           deskTopColor: #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1)
+           primaryColor: #colorLiteral(red: 0.1960784346, green: 0.3411764801, blue: 0.1019607857, alpha: 1),
+           backgroundColor: #colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1)
     ),
     
-    Theme (themePackName: "Sport",
+    Theme (themeName: "Sport",
            emojiPack: ["🏄‍♀️","🏂","⚽️","🏀","🏈","🎾","🏐","🏉","🎱","🏆","🧘🏼‍♀️","🏋🏻‍♂️"],
-           cardBackColor: #colorLiteral(red: 0.521568656, green: 0.1098039225, blue: 0.05098039284, alpha: 1),
-           deskTopColor: #colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1)
+           primaryColor: #colorLiteral(red: 0.521568656, green: 0.1098039225, blue: 0.05098039284, alpha: 1),
+           backgroundColor: #colorLiteral(red: 0.9568627477, green: 0.6588235497, blue: 0.5450980663, alpha: 1)
     ),
-    Theme (themePackName: "Fruit",
+    Theme (themeName: "Fruit",
            emojiPack: ["🍏","🍎","🍐","🍊","🍋","🍌","🍉","🍇","🍓","🍈","🍒","🍑"],
-           cardBackColor: #colorLiteral(red: 0.5058823824, green: 0.3372549117, blue: 0.06666667014, alpha: 1),
-           deskTopColor: #colorLiteral(red: 0.9607843161, green: 0.7058823705, blue: 0.200000003, alpha: 1)
+           primaryColor: #colorLiteral(red: 0.5058823824, green: 0.3372549117, blue: 0.06666667014, alpha: 1),
+           backgroundColor: #colorLiteral(red: 0.9764705896, green: 0.850980401, blue: 0.5490196347, alpha: 1)
     ),
-    Theme (themePackName: "Flags",
+    Theme (themeName: "Flags",
            emojiPack: ["🇺🇸","🇧🇷","🇨🇦","🇨🇺","🇦🇷","🇺🇦","🏴󠁧󠁢󠁳󠁣󠁴󠁿","🇬🇧","🇹🇷","🇺🇾","🇪🇭","🇹🇨"],
-           cardBackColor: #colorLiteral(red: 0.3098039329, green: 0.01568627544, blue: 0.1294117719, alpha: 1),
-           deskTopColor: #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1)
+           primaryColor: #colorLiteral(red: 0.3098039329, green: 0.01568627544, blue: 0.1294117719, alpha: 1),
+           backgroundColor: #colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1)
     ),
-    Theme (themePackName: "Transport",
+    Theme (themeName: "Transport",
            emojiPack: ["🚗","🚕","🚙","🚌","🚎","🏎","🚓","🚑","🚒","🚐","🚚","🚜"],
-           cardBackColor: #colorLiteral(red: 0.09019608051, green: 0, blue: 0.3019607961, alpha: 1),
-           deskTopColor: #colorLiteral(red: 0.2196078449, green: 0.007843137719, blue: 0.8549019694, alpha: 1)
+           primaryColor: #colorLiteral(red: 0.09019608051, green: 0, blue: 0.3019607961, alpha: 1),
+           backgroundColor: #colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)
     ),
     
-    Theme (themePackName: "Food",
+    Theme (themeName: "Food",
            emojiPack: ["🥞","🥓","🥩","🍗","🍖","🌭","🍔","🍟","🍕","🥪","🥙","🌮"],
-           cardBackColor: #colorLiteral(red: 0.1019607857, green: 0.2784313858, blue: 0.400000006, alpha: 1),
-           deskTopColor: #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
+           primaryColor: #colorLiteral(red: 0.1019607857, green: 0.2784313858, blue: 0.400000006, alpha: 1),
+           backgroundColor: #colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1)
     ),]
 
--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -10,14 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var cardBackColor = UIColor.orange
+    var deskTopColor = UIColor.black
+    var emojiPack = ["🧛‍♀️","🧟‍♀️","🎃","🧟‍♂️","🕷","🍭","💀","🤡","👁","🦇","😱","🙀"]
+    var themeName = "Hellowen"
+    
+    
+    var themeIndex = 0 {
+        didSet {
+            themeName = theme[themeIndex].themeName
+            emojiChoices = theme[themeIndex].emojiPack
+            cardBackColor = theme[themeIndex].primaryColor
+            deskTopColor = theme[themeIndex].backgroundColor
+
+            updateTheme()
+        }
+        
+    }
+
+    lazy var emojiChoices = emojiPack
     lazy var game = Concentration (numberOfPairsOfCard: (cardButtons.count + 1)/2)
     
-    lazy var emojiChoices = getRandomTheme()
     var emoji = [Int: String]()
-    
+
+    @IBOutlet weak var themeTitleLabel: UILabel!
     @IBOutlet weak var flipCountLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
     @IBOutlet var cardButtons: [UIButton]!
+    @IBOutlet weak var newGameButton: UIButton!
     
     @IBAction func touchCard(_ sender: UIButton) {
         
@@ -26,7 +46,6 @@ class ViewController: UIViewController {
             updateViewFromModel()
         } else {
             print("Chosen card is out of cardButtons")
-        
         }
 
     }
@@ -34,11 +53,21 @@ class ViewController: UIViewController {
     @IBAction func newGame(_ sender: UIButton) {
         game.resetGame()
         emoji = [Int: String]()
-        emojiChoices = getRandomTheme()
+        themeIndex = theme.count.arc4random
         updateViewFromModel()
         
     }
-
+    
+    func emoji(for card: Card) -> String {
+        if emoji[card.identifier] == nil, emojiChoices.count > 0 {
+            let randomIndex = Int(arc4random_uniform(UInt32(emojiChoices.count)))
+            emoji[card.identifier] = emojiChoices.remove(at: randomIndex)
+            
+        }
+        return emoji[card.identifier] ?? "?"
+        
+    }
+    
     func updateViewFromModel() {
         flipCountLabel.text = "flips: \(game.flips)"
         scoreLabel.text = "score: \(game.score)"
@@ -50,34 +79,20 @@ class ViewController: UIViewController {
                 button.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
             }else {
                 button.setTitle("", for: UIControlState.normal)
-                button.backgroundColor = card.isMatched ? #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 0.1970248288) : #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+                button.backgroundColor = card.isMatched ? #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 0.1970248288) : cardBackColor
             }
         }
             
     }
 
-    func emoji(for card: Card) -> String {
-        if emoji[card.identifier] == nil, emojiChoices.count > 0 {
-            let randomIndex = Int(arc4random_uniform(UInt32(emojiChoices.count)))
-            emoji[card.identifier] = emojiChoices.remove(at: randomIndex)
-        }
-        return emoji[card.identifier] ?? "?"
-    }
-    
-    func getRandomTheme() -> [String] {
-        
-        let    emojiPacks :[String: [String]] = [
-            "Animal":   ["🦆","🐛","🦋","🐝","🐌","🐞","🐬","🐳","🐋","🦈","🐊","🐢"],
-            "Sport":    ["🏄‍♀️","🏂","⚽️","🏀","🏈","🎾","🏐","🏉","🎱","🏆","🧘🏼‍♀️","🏋🏻‍♂️"],
-            "Fruit":    ["🍏","🍎","🍐","🍊","🍋","🍌","🍉","🍇","🍓","🍈","🍒","🍑"],
-            "Flags":    ["🇺🇸","🇧🇷","🇨🇦","🇨🇺","🇦🇷","🇺🇦","🏴󠁧󠁢󠁳󠁣󠁴󠁿","🇬🇧","🇹🇷","🇺🇾","🇪🇭","🇹🇨"],
-            "Food":     ["🥞","🥓","🥩","🍗","🍖","🌭","🍔","🍟","🍕","🥪","🥙","🌮"],
-            "Transport":["🚗","🚕","🚙","🚌","🚎","🏎","🚓","🚑","🚒","🚐","🚚","🚜"]
-        ]
-        
-        let keys = Array(emojiPacks.keys)
-        let result = emojiPacks[keys[keys.count.arc4random]]!
-        return result
+    func updateTheme() {
+        view.backgroundColor = deskTopColor
+        themeTitleLabel.text = themeName
+        themeTitleLabel.textColor = cardBackColor
+        scoreLabel.textColor = cardBackColor
+        flipCountLabel.textColor = cardBackColor
+        newGameButton.tintColor = UIColor.black
+        newGameButton.setTitleColor(cardBackColor, for: .normal)
     }
     
 }
